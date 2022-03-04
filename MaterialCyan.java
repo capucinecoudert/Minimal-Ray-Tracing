b1f59f0b14d7382188c41a417e84ec868933db74
nninnonn <ninon.delamarre@gmail.com>
2022-03-04 17:44:00 +0100

    add material classes

---
+++ MaterialCyan.java
@@ -0,0 +1,9 @@
+public class MaterialCyan extends Material {
+    
+    public MaterialCyan (){
+        reflectionCoeff = 0.5;
+        specularPower = 60;
+        specularColor = new Color(1.0, 1.0, 1.0);
+        diffusionColor = new Color(0.0, 1.0, 1.0);
+    }
+}

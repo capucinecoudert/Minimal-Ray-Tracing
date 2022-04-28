922346f69f3d36acbb67f38a54cbdee703000c42
Capucine Coudert <coudert.capucine@gmail.com>
2022-04-28 02:20:11 +0200

    suppr class material

--- MaterialBlack.java
+++
@@ -1,9 +0,0 @@
-/*public class MaterialBlack extends Material {
-    
-    public MaterialBlack(){
-        reflectionCoeff = 1.0;
-        specularPower = 60;
-        //specularColor = new Color(1.0, 1.0, 1.0);
-        diffusionColor = new Color(0.01, 0.01, 0.01);
-    }
-}*/
